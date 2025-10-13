ca62ef6b4484861657ed025643174621203306d4
Akshayar07 <rakshaya22@gmail.com>
2025-10-14 03:21:24 +0530

    Basic layou impl.

--- app/src/main/java/com/example/calcx/screens/landing/landingScreen.kt
+++ app/src/main/java/com/example/calcx/screens/landing/landingScreen.kt
@@ -1,9 +1,15 @@
 package com.example.calcx.screens.landing
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -11,6 +17,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -33,12 +40,8 @@ fun LandingScreen() {
                         Icon(imageVector = Icons.Default.Favorite, contentDescription = "Favorite")
                     }
                 }
-
-
             )
         }
-
-
     ) { innerPadding ->
         Column(
             modifier = Modifier
@@ -51,6 +54,69 @@ fun LandingScreen() {
                 )
             )
             Text(text = "Welcome Back!")
+
+            Box(modifier = Modifier
+                .weight(1f)
+                .height(100.dp)) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxSize(),
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "Age")
+                        }
+                    }
+                    Box(modifier = Modifier.weight(1f)) {
+
+
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            Card(modifier = Modifier.height(100.dp)) {
+                                Box(
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                        .fillMaxSize(),
+
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(text = "Bmi")
+                                }
+                            }
+                            Card(modifier = Modifier.height(100.dp)) {
+                                Box(
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                        .fillMaxSize(),
+
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(text = "Bmi")
+                                }
+                            }
+
+
+                        }
+                    }
+
+                    //
+
+
+                }
+            }
+
         }
     }
 
